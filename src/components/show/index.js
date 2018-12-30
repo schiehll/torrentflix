06e006fa8f8025552ed4c831dc39724d98e6380e
Reinaldo Schiehll <rn.schiehll@gmail.com>
2018-12-30 08:41:04 -0200

    Add styles to show page

--- src/components/show/index.js
+++ src/components/show/index.js
@@ -1,93 +1,129 @@
-import React, { useState } from 'react'
+import React, { Fragment, useState } from 'react'
 import usePost from 'hooks/usePost'
 import requestCast from 'utils/requestCast'
+import Message from 'components/message'
+import Loader from 'components/loader'
+import * as S from './styles'
 
 const Show = ({ show }) => {
-  const [selectedSeason, setSelectedSeason] = useState(1)
-  const [selectedEpisode, setSelectedEpisode] = useState(1)
-  const [quality, setQuality] = useState('NONE')
-
-  const season = show.seasons.find(
-    season => season.number.toString() === selectedSeason.toString()
-  )
-
-  const episode = season.episodes.find(
-    episode => episode.number.toString() === selectedEpisode.toString()
-  )
-
-  const magnetLink = episode.torrents.find(
-    torrent => torrent.quality === quality
-  )
+  const [selectedSeason, setSelectedSeason] = useState(null)
+  const [selectedEpisode, setSelectedEpisode] = useState(null)
+  const [torrentInfo, setTorrentInfo] = useState(null)
 
   const { data, loading } = usePost(
-    magnetLink ? `${process.env.API_URL}/start` : null,
-    { torrentId: magnetLink?.url }
+    torrentInfo ? `${process.env.API_URL}/start` : null,
+    { torrentId: torrentInfo?.url }
   )
 
-  const handleSeasonChange = e => {
-    setSelectedSeason(e.target.value)
-    setSelectedEpisode(1)
-    setQuality('NONE')
+  const handleSeasonChange = season => {
+    setSelectedSeason(season)
+    setSelectedEpisode(null)
+    setTorrentInfo(null)
   }
 
-  const handleEpisodeChange = e => {
-    setSelectedEpisode(e.target.value)
-    setQuality('NONE')
+  const handleEpisodeChange = episode => {
+    setSelectedEpisode(episode)
+    setTorrentInfo(null)
   }
 
-  const handleQualityChange = e => {
-    setQuality(e.target.value)
+  const handleTorrentInfoChange = link => {
+    setTorrentInfo(link)
   }
 
   const cast = () => {
-    if (magnetLink && data && data.url && !loading) {
+    if (torrentInfo && data && data.url && !loading) {
       requestCast(data.url)
     }
   }
 
   return (
-    <div>
-      <label>
-        Season
-        <select value={selectedSeason} onChange={handleSeasonChange}>
-          {show.seasons.map(({ number }) => {
-            return (
-              <option key={number} value={number}>
-                {number}
-              </option>
-            )
-          })}
-        </select>
-      </label>
-      <label>
-        Episode
-        <select value={selectedEpisode} onChange={handleEpisodeChange}>
-          {season.episodes.map(({ number }) => {
-            return (
-              <option key={number} value={number}>
-                {number}
-              </option>
-            )
-          })}
-        </select>
-      </label>
-      <label>
-        Torrent
-        <select value={quality} onChange={handleQualityChange}>
-          <option value="NONE">Select one</option>
-          {episode.torrents.map((torrent, key) => {
-            return (
-              <option key={key} value={torrent.quality}>
-                {torrent.quality}
-              </option>
-            )
-          })}
-        </select>
-      </label>
-      <pre>{JSON.stringify(episode, null, 2)}</pre>
-      {loading && 'loading'}
-      <button onClick={cast}>CAST</button>
-    </div>
+    <S.Content>
+      <h2>{`🎬 ${show.title}`}</h2>
+      <p>{show.synopsis}</p>
+      {loading && (
+        <Message emoji={<Loader size="big" />} text="getting cast link..." />
+      )}
+      {!loading && (
+        <S.List>
+          {selectedSeason === null &&
+            show.seasons.map(season => {
+              return (
+                <S.Item
+                  onClick={() => handleSeasonChange(season)}
+                  key={`season-${season.number}`}
+                >
+                  <S.Infos>
+                    Season {season.number.padStart(2, '0')}{' '}
+                    <span>&middot; {season.episodes.length} episodes</span>
+                  </S.Infos>
+                  <span>👉</span>
+                </S.Item>
+              )
+            })}
+          {selectedSeason !== null &&
+            !selectedEpisode && (
+              <Fragment>
+                <S.ItemButton onClick={() => handleSeasonChange(null)}>
+                  <span>👈</span>
+                  <S.Infos>Go back to season selection</S.Infos>
+                </S.ItemButton>
+                {selectedSeason.episodes.map(episode => (
+                  <S.Item
+                    onClick={() => handleEpisodeChange(episode)}
+                    key={`episode-${episode.number}`}
+                  >
+                    <S.MultiLineInfos>
+                      {episode.title}
+                      <span>
+                        S{selectedSeason.number.padStart(2, '0')}E
+                        {episode.number.padStart(2, '0')}
+                      </span>
+                    </S.MultiLineInfos>
+                    <span>👉</span>
+                  </S.Item>
+                ))}
+              </Fragment>
+            )}
+          {selectedEpisode && (
+            <Fragment>
+              <S.ItemButton onClick={() => handleEpisodeChange(null)}>
+                <span>👈</span>
+                <S.Infos>Go back to episode selection</S.Infos>
+              </S.ItemButton>
+              {selectedEpisode.torrents.map(torrent => (
+                <S.Item
+                  onClick={() => handleTorrentInfoChange(torrent)}
+                  key={`torrent-${torrent.quality}`}
+                >
+                  <S.MultiLineInfos>
+                    {torrent.quality}
+                    <span>{torrent.provider}</span>
+                  </S.MultiLineInfos>
+                  <span>👉</span>
+                </S.Item>
+              ))}
+            </Fragment>
+          )}
+          {data &&
+            data.url &&
+            torrentInfo && (
+              <S.ItemButton onClick={cast}>
+                <span>🤞</span>
+                <S.Infos
+                  dangerouslySetInnerHTML={{
+                    __html: `Cast S${selectedSeason.number.padStart(
+                      2,
+                      '0'
+                    )}E${selectedEpisode.number.padStart(2, '0')} &middot; ${
+                      torrentInfo.quality
+                    } &middot; ${torrentInfo.provider}`
+                  }}
+                />
+              </S.ItemButton>
+            )}
+        </S.List>
+      )}
+    </S.Content>
   )
 }
 
